59b069f34dbdee4f80150fa211f75e8a5c9c06e1
Student 1 <student1@NIBMADMIN01s-iMac.local>
2026-07-01 14:50:19 +0530

    Update QuizService by adding trivia API

--- ios practicals/ios practicals/Game3- QuizRush /QuizService.swift
+++ ios practicals/ios practicals/Game3- QuizRush /QuizService.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct QuizService {
+
+    // API URL for simple questions
+    private let urlString = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+
+    // fetch questions from the API
+    func fetchQuestions() async throws -> [Question] {
+
+        // create the URL
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        // download data
+        let (data, _) = try await URLSession.shared.data(from: url)
+
+        // Decode JSON into Swift models
+        let response = try JSONDecoder().decode(TriviaResponse.self, from: data)
+
+        // return only the questions
+        return response.results
+    }
+}
