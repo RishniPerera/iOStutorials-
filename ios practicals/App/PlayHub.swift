a4fe76e1fcfcebd4957b2c04c677e2200b9bb063
Student 1 <student1@NIBMADMIN01s-iMac.local>
2026-07-16 12:48:25 +0530

    Renamed app entry file to PlayHub

--- ios practicals/App/PlayHub.swift
+++ ios practicals/App/PlayHub.swift
@@ -1,5 +1,5 @@
 //
-//  ios_practicalsApp.swift
+//  PlayHub.swift
 //  ios practicals
 //
 //  Created by Student 1 on 2026-06-06.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct ios_practicalsApp: App {
+struct PlayHub: App {
     @AppStorage("darkMode") var darkMode = false
     @StateObject var gameSession = GameSession()
     @StateObject var locationService = LocationService()
